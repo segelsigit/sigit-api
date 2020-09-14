e4f85a3b407d6169028212f6e1edd113c95b90c0
galrose <bengal.rosenberg@gmail.com>
2020-09-14 10:06:35 +0300

    added cors

--- factory.py
+++ factory.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from api import student_api
+from flask_cors import CORS
 
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     app.register_blueprint(student_api.student)
 
     return app
